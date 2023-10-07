ec1f985bd8e0b8790a3566eff3c35d84bc3fc14b
AlpacaOnly <ayazhan1121@gmail.com>
2023-10-08 00:17:15 +0600

    init

---
+++ Chess/ChessMain.py
@@ -0,0 +1,96 @@
+"""
+gets user input and displaying current state objects
+"""
+
+import pygame as p
+from Chess import ChessEngine
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+SQ_SIZE = HEIGHT / DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+
+
+def loadImages():
+    pieces = ["bR", "bN", "bB", "bQ", "bK", "wR", "wN", "wB", "wQ", "wK", "wp", "bp"]
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = ChessEngine.GameState()
+    loadImages()
+    running = True
+    sqSelected = ()  # last click of user (tuple(row, col))
+    playerClicks = []  # keep track of user clicks ((two tuples:[(6,4), (5,4)])
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()  # x, y location of mouse
+                c = location[0]//SQ_SIZE
+                r = location[1]//SQ_SIZE
+                if sqSelected == (r, c):
+                    sqSelected = ()
+                    playerClicks = []
+                else:
+                    sqSelected = (r, c)
+                    playerClicks.append(sqSelected)
+                if len(playerClicks) == 2:
+                    move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    print(move.getChessNotation())
+                    gs.makeMove(move)
+                    sqSelected = ()  # reset userClicks
+                    playerClicks = []
+
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+
+'''
+Responsible for all the graphics within a current game state
+'''
+
+
+def drawGameState(screen, gs):
+    drawBoard(screen)
+    drawPieces(screen, gs.board)
+
+
+'''
+Draw the squares on the board. The top left square is always light.
+'''
+
+
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("gray")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[(r + c) % 2]
+            p.draw.rect(screen, color, p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+'''
+Draw the pieces on the board using the current GameState.board
+'''
+
+
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
